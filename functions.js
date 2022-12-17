fda67ac2b7009a1491de69664a82c312227ca929
HDBarton <heatherdbarton@gmail.com>
2022-12-16 23:55:49 -0700

    Add skeleton functions based on requirements

--- functions.js
+++ functions.js
@@ -1,1 +1,18 @@
-//Game functionality and user interaction goes in here
+// ry to have as little global code as possible
+
+const Gameboard = () =>{
+    board = [];
+};
+
+const Player = (() =>{
+
+})();
+
+const GameActions = (() =>{
+
+})();
+
+// renders gameboard array to webpage
+const DisplayController = () =>{
+
+};
