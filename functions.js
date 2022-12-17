2f8c43a0679b3b5c0ec5fc00adea8fc46591a695
HDBarton <heatherdbarton@gmail.com>
2022-12-17 00:12:58 -0700

    Add requirement notes for each function

--- functions.js
+++ functions.js
@@ -1,13 +1,20 @@
-// ry to have as little global code as possible
+// Try to have as little global code as possible
 
+// Build the logic that checks for when the game is over! Should check for 3-in-a-row and a tie.
 const Gameboard = () =>{
     board = [];
 };
 
+// allow players to put in their names
 const Player = (() =>{
 
 })();
 
+// Build the functions that allow players to add marks to a specific spot on the board, and then tie it to the DOM, 
+// letting players click on the gameboard to place their marker. 
+// Don’t forget the logic that keeps players from playing in spots that are already taken!
+// include a button to start/restart the game 
+// and add a display element that congratulates the winning player
 const GameActions = (() =>{
 
 })();
@@ -16,3 +23,5 @@ const GameActions = (() =>{
 const DisplayController = () =>{
 
 };
+
+//Optional work for later: create an AI so that a player can play against the computer
