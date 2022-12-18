7f8373c1c96bc139234f7f2d4390cd3f8838b0d9
HDBarton <heatherdbarton@gmail.com>
2022-12-18 16:19:17 -0700

    Add functionality to add player 1 name to page

--- functions.js
+++ functions.js
@@ -1,12 +1,20 @@
 // Try to have as little global code as possible
-players = 0;
+const players = 0;
 
+// heper fucntions
 function openPlayerBox() {
   document.getElementById("add-players-form").style.display = "block";
 }
 // TODO: need to make sure button remains if required content isn't filled in
 function closeForm(){
     document.getElementById("close-form").style.display = "none";
+    let placeForPlayerNames = document.getElementById("addPlayerNamesHere");
+    let playersText = `Player X: ${DisplayController.player1}`;
+    console.log(playersText);
+    placeForPlayerNames.appendChild(document.createTextNode(playersText));
+}
+function displayPlayer1(){
+    Player.printPlayer1;
 }
 
 // Build the logic that checks for when the game is over! Should check for 3-in-a-row and a tie.
@@ -16,12 +24,22 @@ const Gameboard = () =>{
 
 // allow players to put in their names
 const Player = ((name) =>{
-    players +=1;
+    // get player names
+    let form = document.getElementById("form");
+    const printPlayer1 = () => {
+        // TODO: change for whichever works: this, j=orjust directly pulling from DisplayController
+        console.log(DisplayController.player1);
+        let addHere = document.createElement("div");
+        addHere.appendChild(DisplayController.player1);
+    }
+
+    //players +=1;
     let playerToken = "";
     if (players == 1){
       playerToken = "X";
     }else {
         playerToken = "0";
+    }
     this.sayPlayer = `Player ${playerToken}: ${name}`;
 })();
 
@@ -35,8 +53,10 @@ const GameActions = (() =>{
 })();
 
 // renders gameboard array to webpage
-const DisplayController = () =>{
+const DisplayController = (() =>{
+    // get players names
+    const player1 = () => form.elements["player1"];
+    const player2 = () => form.elements["player2"];
+})();
 
-};
-
-//Optional work for later: create an AI so that a player can play against the computer
+// Optional work for later: create an AI so that a player can play against the computer
