cba0cadf6353f9a34f4b361ecf1ee851965591e6
HDBarton <heatherdbarton@gmail.com>
2023-01-07 00:41:49 -0700

    Add test input button

--- functions.js
+++ functions.js
@@ -5,19 +5,25 @@
 // 3.) Create a mechanism to check for a win after each click
 // 4.) Create mechansim to check that an action hasn't already been played on the square being clicked and an alert if so
 // 5.) OPTIONAL: Keep game from starting until names have been entered
-const players = 0;
+const currentPlayer1 = "";
+const currentPlayer2 = "";
+const player1Section = document.getElementById('player1box');
+const player2Section = document.getElementById('player2box');
+player1Section.innerHTML = currentPlayer1;
+player2Section.innerHTML = currentPlayer2;
+const testArea = document.getElementById("test-button");
+testArea.onclick = function() {showMe()};
+const onClose = document.getElementById("close-form");
+onClose.onclick = function() {DisplayController.addPlayer1(), DisplayController.addPlayer2(), closeForm()};
 
 // helper fucntions
 function openPlayerBox() {
   document.getElementById("add-players-form").style.display = "block";
 }
 // TODO: need to make sure button remains if required content isn't filled in
+/////////////////////WAS WORKING ON THIS: Names seem to blink after submission but immediately disappear
 function closeForm(){
-    document.getElementById("close-form").style.display = "none";
-    let placeForPlayerNames = document.getElementById("addPlayerNamesHere");
-    let playersText = `Player X: ${DisplayController.player1}`;
-    console.log(playersText);
-    placeForPlayerNames.appendChild(document.createTextNode(playersText));
+    document.getElementById("add-players-form").style.display = "none";
 }
 function displayPlayer1(){
     Player.printPlayer1;
@@ -30,24 +36,24 @@ const Gameboard = () =>{
 };
 
 // allow players to put in their names
-const Player = ((name) =>{
-    // get player names
-    const printPlayer1 = () => {
-        // TODO: change for whichever works: this, or just directly pulling from DisplayController
-        console.log(DisplayController.player1);
-        let addHere = document.createElement("div");
-        addHere.appendChild(DisplayController.player1);
-    }
+// const Player = ((name) =>{
+//     // get player names
+//     const printPlayer1 = () => {
+//         // TODO: change for whichever works: this, or just directly pulling from DisplayController
+//         console.log(DisplayController.player1);
+//         let addHere = document.createElement("div");
+//         addHere.appendChild(DisplayController.player1);
+//     }
 
-    //players +=1;
-    let playerToken = "";
-    if (players == 1){
-      playerToken = "X";
-    }else {
-        playerToken = "0";
-    }
-    this.sayPlayer = `Player ${playerToken}: ${name}`;
-})();
+//     //players +=1;
+//     let playerToken = "";
+//     if (players == 1){
+//       playerToken = "X";
+//     }else {
+//         playerToken = "0";
+//     }
+//     this.sayPlayer = `Player ${playerToken}: ${name}`;
+// })();
 
 // Build the functions that allow players to add marks to a specific spot on the board, and then tie it to the DOM, 
 // letting players click on the gameboard to place their marker. 
@@ -60,23 +66,48 @@ const GameActions = (() =>{
 
 // renders gameboard array to webpage
 // SUGGESTED MODULE
+
+
 const DisplayController = (() => {
     // get players names
     const form = document.getElementById("form");
     const player1Section = document.getElementById('player1box');
     const player2Section = document.getElementById('player2box');
-    const player1name = document.createTextNode("Joe");
-    const player2name = document.createTextNode("Susan");
-    const player1 = () => form.elements["player1"];
-    const player2 = () => form.elements["player2"];
-    const addPlayer1 = () => player1Section.appendChild(player1name);
-    const addPlayer2 = () => player2Section.appendChild(player2name);
+    const addPlayer1 = () => {
+        let player1element = form.elements["player1"];
+        let player1name = String(player1element.value);
+        let player1 = document.createTextNode(player1name);
+        player1Section.appendChild(player1);
+        console.log(player1name);
+        currentPlayer1 = player1name;
+    }
+    const addPlayer2 = () => {
+        let player2element = form.elements["player2"];
+        let player2name = String(player2element.value);
+        let player2 = document.createTextNode(player2name);
+        player2Section.appendChild(player2);
+        console.log(player2name);
+        currentPlayer2 = player2name;
+    }
+    const showMe = () => {
+        let text = document.createTextNode("Here I am!");
+        let insertHere = document.getElementById("test");
+        insertHere.appendChild(text);
+    }
     return {
         addPlayer1,
-        addPlayer2
+        addPlayer2,
+        showMe
     };
 })();
 
-DisplayController.addPlayer1();
-DisplayController.addPlayer2();
+
+function showMe() {
+    let text = document.createTextNode("Here I am!");
+    let insertHere = document.getElementById("test");
+    insertHere.appendChild(text);
+}
+
+
+
 // Optional work for later: create an AI so that a player can play against the computer
