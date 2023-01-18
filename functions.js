09411c74aca27549cb1c7334ddccb934f6a9d354
HDBarton <heatherdbarton@gmail.com>
2023-01-17 23:27:30 -0700

    Update notes and TODOs

--- functions.js
+++ functions.js
@@ -1,10 +1,9 @@
 // Try to have as little global code as possible
 // TODO:
-//////////////////Currently working on checking for a win////////////////////
-// 3.) Create a mechanism to check for a win after each click
-// 4.) Create mechansim to check that an action hasn't already been played on the square being clicked and an alert if so
-// 5.) Create a function to clear the board when new players are entered
-// 6.) OPTIONAL: Keep game from starting until names have been entered
+// 5.) Create a function to clear the board, winner section, and player names when new playername form is opened
+// 6.) Add a button for another round under the gameboard which clears the board and winner section but not player names
+// 7.) Add a button at the bottom for Clear All which also clears player names
+// 8.) OPTIONAL: Keep game from starting until names have been entered
 
 ////////////////////ONCLICK FUNCTIONALITIES (can't seem to be used from within Factory or Module Functions)///////////////////
 //let turn = "X";
@@ -162,36 +161,6 @@ const Gameboard = () =>{
 ////////////////////////START GAMEBOARD//////////////////////////////
 const thisGameBoard = Gameboard();
 
-/////////////////////////PREVIOUS PLAYER FACTORY FUNCTION//////////////////////////////
-// function displayPlayer1(){
-//     Player.printPlayer1;
-// }
-
-// allow players to put in their names
-// const Players = ((name) =>{
-//     // get player names
-//     const printPlayer1 = () => {
-//         // TODO: change for whichever works: this, or just directly pulling from DisplayController
-//         console.log(DisplayController.player1);
-//         let addHere = document.createElement("div");
-//         addHere.appendChild(DisplayController.player1);
-//     }
-
-//     //players +=1;
-//     let playerToken = "";
-//     if (players == 1){
-//       playerToken = "X";
-//     }else {
-//         playerToken = "0";
-//     }
-//     this.sayPlayer = `Player ${playerToken}: ${name}`;
-// })();
-
-// Build the functions that allow players to add marks to a specific spot on the board, and then tie it to the DOM, 
-// letting players click on the gameboard to place their marker. 
-// Don’t forget the logic that keeps players from playing in spots that are already taken!
-// include a button to start/restart the game 
-// and add a display element that congratulates the winning player
 const GameActions = () => {
     let turn = "O";
     const changePlayer = () => {
@@ -236,6 +205,12 @@ const DisplayController = (() => {
     };
     const getPlayer1Name = () => currentPlayer1;
     const getPlayer2Name = () => currentPlayer2;
+    ////////////////////////////////////////////////////CREATING BOARD CLWARING FUNCTIONALITY/////////////////////////////////
+    const clearBoardDisplay = () => {
+        for(i = 0; i < 9; i++){
+
+        }
+    };
     /////////////////ONCLICK appears not to function from within a Factory Function//////////////////////////
     // const closePopUp = () => {
     //     const onClose = document.getElementById("close-form");
@@ -264,39 +239,5 @@ function showMe() {
     let insertHere = document.getElementById("test");
     insertHere.appendChild(text);
 }
-
-/////////////////////////////////////FORMER DISPLAY CONTROLLER/////////////////////////////////////////
-// const DisplayController = (() => {
-//     // get players names
-//     const form = document.getElementById("form");
-//     const player1Section = document.getElementById('player1box');
-//     const player2Section = document.getElementById('player2box');
-//     const addPlayer1 = () => {
-//         let player1element = form.elements["player1"];
-//         let player1name = String(player1element.value);
-//         let player1 = document.createTextNode(player1name);
-//         player1Section.appendChild(player1);
-//         console.log(player1name);
-//         currentPlayer1 = player1name;
-//     }
-//     const addPlayer2 = () => {
-//         let player2element = form.elements["player2"];
-//         let player2name = String(player2element.value);
-//         let player2 = document.createTextNode(player2name);
-//         player2Section.appendChild(player2);
-//         console.log(player2name);
-//         currentPlayer2 = player2name;
-//     }
-//     const showMe = () => {
-//         let text = document.createTextNode("Z");
-//         let insertHere = document.getElementById("square9");
-//         insertHere.appendChild(text);
-//     }
-//     return {
-//         addPlayer1,
-//         addPlayer2,
-//         showMe
-//     };
-// })();
 
 // Optional work for later: create an AI so that a player can play against the computer
